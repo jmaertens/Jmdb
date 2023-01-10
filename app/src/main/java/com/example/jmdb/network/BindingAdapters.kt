4a55ab267b0582879e0a688ec7f067b8442482b4
Jonas Maertens <jonas.maertens@student.hogent.be>
2023-01-10 19:29:31 +0100

    Display loading and broken image

--- app/src/main/java/com/example/jmdb/network/BindingAdapters.kt
+++ app/src/main/java/com/example/jmdb/network/BindingAdapters.kt
@@ -4,8 +4,10 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.jmdb.R
 
-    //Convert imgUrl to a URI with the https scheme
+//Convert imgUrl to a URI with the https scheme
     @BindingAdapter("imageUrl")
     fun bindImage(imgView: ImageView, imgUrl: String?) {
         imgUrl?.let {
@@ -13,6 +15,10 @@ import com.bumptech.glide.Glide
             val baseUrl = "https://image.tmdb.org/t/p/original"
             Glide.with(imgView.context)
                 .load(baseUrl + imgUri)
+                .apply(
+                    RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image))
                 .into(imgView)
         }
     }

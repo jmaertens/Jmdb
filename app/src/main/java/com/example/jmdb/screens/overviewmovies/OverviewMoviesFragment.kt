0936f571dfdca701a2f0db6b1ee99193e290b4d3
Jonas Maertens <jonas.maertens@student.hogent.be>
2023-01-14 17:03:21 +0100

    Update layout

--- app/src/main/java/com/example/jmdb/screens/overviewmovies/OverviewMoviesFragment.kt
+++ app/src/main/java/com/example/jmdb/screens/overviewmovies/OverviewMoviesFragment.kt
@@ -1,21 +1,16 @@
 package com.example.jmdb.screens.overviewmovies
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import com.example.jmdb.R
 import com.example.jmdb.databinding.FragmentOverviewMoviesBinding
-import com.example.jmdb.databinding.GridViewItemBinding
-import android.view.*
-import androidx.navigation.Navigation
+
 
 class OverviewMoviesFragment : Fragment() {
 
@@ -30,14 +25,18 @@ class OverviewMoviesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         binding = FragmentOverviewMoviesBinding.inflate(inflater)
+
+
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
 
         //Set binding.Postergridadapter to a new postergridadapter
         binding.posterGrid.adapter = PosterGridAdapter(PosterGridAdapter.OnClickListener {
@@ -52,8 +51,7 @@ class OverviewMoviesFragment : Fragment() {
             }
         })
 
-
-
         return binding.root
     }
+
 }

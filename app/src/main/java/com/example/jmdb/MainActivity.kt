84a7486c64bf8ddb5bfc19751a3b197a5559a6ca
Jonas Maertens <jonas.maertens@student.hogent.be>
2023-01-15 14:12:50 +0100

    Fix navigation drawer

--- app/src/main/java/com/example/jmdb/MainActivity.kt
+++ app/src/main/java/com/example/jmdb/MainActivity.kt
@@ -27,56 +27,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         setContentView( R.layout.activity_main)
-
 
         var navView : NavigationView = findViewById(R.id.navView)
 
         //use material toolbar instead of default app bar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         val navController = this.findNavController(R.id.navHostFragment)
-
         drawerLayout = findViewById(R.id.drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-
-
-
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-
-
-
         NavigationUI.setupWithNavController(navView, navController)
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
 }
